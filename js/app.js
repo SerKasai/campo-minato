4323fedae86eea0d14f87059bdca9f4fc3d10505
SerKasai <serjtheignis92@gmail.com>
2025-05-28 23:46:18 +0200

    commit 1

--- js/app.js
+++ js/app.js
@@ -1,17 +1,31 @@
 console.log('Campo Minato')
 
+function generaElencoNumeri(start, end) {
+  const elenco = [];
+  for (let i = start; i <= end; i++) {
+    elenco.push(i);
+    // console.log(elenco)
+  }
+  return elenco;
+}
 const numeri = []
-const numeroColonne = [10, 9, 7];
-const numeroRighe = [10, 9, 7];
+const numeroColonne = 10
+const numeroRighe = 10
 const difficoltà = ['Easy', 'Hard', 'Crazy'];
 const celleTotali = numeroRighe * numeroColonne;
 
 
-for (let i = 0; i < numeri.length; i++) {
-  const numero = numeri[i];
+const n = generaElencoNumeri(1, celleTotali);
+
+// numeri.push(n);
+console.log(n);
+const gridElement = document.querySelector('.grid');
+
+for (let i = 0; i < n.length; i++) {
+  const numero = n[i];
   const element = document.createElement('div');
   element.classList.add('square');
-  element.style.width = `calc( 100% / ${numeroColonne})`
+  element.style.width = 'calc(100% / 10)'
   element.append(numero);
 
   gridElement.appendChild(element);
